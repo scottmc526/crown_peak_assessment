10162e971b3b7f69021963c3f61dadc4bcc20399
Scott Christensen <scottmc526@gmail.com>
2016-07-07 15:56:20 -0600

    works for numbers up to 10.09, doesnt seem to work if decimal is greater than .09

--- spec/printNumber_spec.js
+++ spec/printNumber_spec.js
@@ -1,18 +1,19 @@
 var myFuncs = require('../lib/printNumber');
-// tests for basic numbers
+// tests for basic numbers and format validation
 describe('intToEnglish', function() {
-  xit('should return the word one when passed the number 1', function() {
+  it('should return the word one when passed the number 1', function() {
     var num = 1;
-    expect(myFuncs.intToEnglish(num)).toEqual('one');
+    expect(myFuncs.intToEnglish(num)).toEqual('one and 00/100');
   })
 })
 
 describe('intToEnglish', function() {
-  xit('should return the word ten when passed the number 10', function() {
+  it('should return the word ten when passed the number 10', function() {
     var num = 10;
-    expect(myFuncs.intToEnglish(num)).toEqual('ten')
+    expect(myFuncs.intToEnglish(num)).toEqual('ten and 00/100')
   })
 })
+
 //test to make sure decimal function is correct
 describe('decimal', function() {
   it('should return the decimal number over 100', function() {
