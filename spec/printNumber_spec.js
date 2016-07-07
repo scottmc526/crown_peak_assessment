7cc8ebd34e5cd8331a82c7db933b25425b7cd1a1
Scott Christensen <scottmc526@gmail.com>
2016-07-07 14:36:06 -0600

    function works for numbers 0-10

--- spec/printNumber_spec.js
+++ spec/printNumber_spec.js
@@ -1,8 +1,15 @@
 var myFuncs = require('../lib/printNumber');
 
 describe('intToEnglish', function() {
+  it('should return the word one when passed the number 1', function() {
+    var num = 1;
+    expect(myFuncs.intToEnglish(num)).toEqual('one');
+  })
+})
+
+describe('intToEnglish', function() {
   it('should return the word ten when passed the number 10', function() {
     var num = 10;
-    expect(myFuncs.intToEnglish(num)).toEqual('ten');
+    expect(myFuncs.intToEnglish(num)).toEqual('ten')
   })
 })
