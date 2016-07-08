859b438a20112168177d45b7a6d5f18f50d0df79
Scott Christensen <scottmc526@gmail.com>
2016-07-07 22:33:06 -0600

    function works for numbers up to 99.99

--- spec/printNumber_spec.js
+++ spec/printNumber_spec.js
@@ -2,51 +2,60 @@ var myFuncs = require('../lib/printNumber');
 // tests for basic numbers and format validation
 describe('intToEnglish', function() {
   it('should return the word one when passed the number 1', function() {
-    var num = 1;
-    expect(myFuncs.intToEnglish(num)).toEqual('one and 00/100');
+    expect(myFuncs.intToEnglish(1)).toEqual('one and 00/100');
   })
 })
 describe('intToEnglish', function() {
   it('should return the word ten when passed the number 10', function() {
-    var num = 10;
-    expect(myFuncs.intToEnglish(num)).toEqual('ten and 00/100')
+    expect(myFuncs.intToEnglish(10)).toEqual('ten and 00/100')
+  })
+})
+describe('intToEnglish', function() {
+  it('should return the word fifteen and 40/100 when passed the number 15.4', function() {
+    expect(myFuncs.intToEnglish(15.4)).toEqual('fifteen and 40/100')
+  })
+})
+describe('intToEnglish', function() {
+  it('should round the number 12.447 to 12.45 and return twelve and 45/100', function() {
+    expect(myFuncs.intToEnglish(12.447)).toEqual('twelve and 45/100')
   })
 })
 
 //test to determine if decimal function for decimal under .10 is correct
 describe('decimal', function() {
   it('should return the decimal number over 100', function() {
-    var decimal = '.04';
-    expect(myFuncs.handleDecimal(decimal)).toEqual('04/100')
+    expect(myFuncs.handleDecimal('.04')).toEqual('04/100')
   })
 })
 
 //tests determine if decimal funtion for decimal at or over .10 is correct
 describe('decimal', function() {
   it('should return the decimal number over 100', function() {
-    var decimal = '.10';
-    expect(myFuncs.handleDecimal(decimal)).toEqual('10/100')
+    expect(myFuncs.handleDecimal('.10')).toEqual('10/100')
   })
 })
 describe('decimal', function() {
   it('should return the decimal number over 100', function() {
-    var decimal = '.11';
-    expect(myFuncs.handleDecimal(decimal)).toEqual('11/100')
+    expect(myFuncs.handleDecimal('.11')).toEqual('11/100')
   })
 })
 
 //test to determine if number under 20 with decimal that doesn't end in 0 is correct
 describe('intToEnglish', function() {
   it('should return ten and 04/100 when passed the number 10.04', function() {
-    var num = 10.04;
-    expect(myFuncs.intToEnglish(num)).toEqual('ten and 04/100')
+    expect(myFuncs.intToEnglish(10.04)).toEqual('ten and 04/100')
   })
 })
 
 //test to determine if number under 20 with decimal that ends with 0 is correct
 describe('intToEnglish', function() {
   it('should return ten and 50/100 when passed the number 10.50', function() {
-    var num = 10.50;
-    expect(myFuncs.intToEnglish(num)).toEqual('ten and 50/100')
+    expect(myFuncs.intToEnglish(10.50)).toEqual('ten and 50/100')
+  })
+})
+//test to determine if number over 20 and less than 100
+describe('intToEnglish', function() {
+  it('should return 65 and 33/100 when passed the number 65.33', function() {
+    expect(myFuncs.intToEnglish(65.33)).toEqual('sixty-five and 33/100')
   })
 })
